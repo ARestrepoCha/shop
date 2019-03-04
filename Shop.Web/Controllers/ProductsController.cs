c8e1173c04a5ebf94a288f04a53f4247dbc1fbce
ALEJANDRO RESTREPO CHAVARRIAGA <alejo0704@gmail.com>
2019-03-03 23:08:59 -0500

    Add images in products and create the view model of products

--- Shop.Web/Controllers/ProductsController.cs
+++ Shop.Web/Controllers/ProductsController.cs
@@ -6,6 +6,9 @@ namespace Shop.Web.Controllers
     using Shop.Web.Data;
     using Shop.Web.Data.Entities;
     using Shop.Web.Helpers;
+    using Shop.Web.Models;
+    using System;
+    using System.IO;
     using System.Threading.Tasks;
 
     public class ProductsController : Controller
@@ -52,17 +55,51 @@ namespace Shop.Web.Controllers
         // POST: Products/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(Product product)
+        public async Task<IActionResult> Create(ProductViewModel view)
         {
             if (ModelState.IsValid)
             {
+                var path = string.Empty;
+
+                if (view.ImageFile != null && view.ImageFile.Length > 0)
+                {
+                    path = Path.Combine(Directory.GetCurrentDirectory(), 
+                        "wwwroot\\images\\Products", 
+                        view.ImageFile.FileName);
+
+                    using (var stream = new FileStream(path, FileMode.Create))
+                    {
+                        await view.ImageFile.CopyToAsync(stream);
+                    }
+
+                    path = $"~/images/Products/{view.ImageFile.FileName}";
+                }
+
+                var product = this.ToProduct(view, path);
+
                 // TODO: Pending to change to: this.User.Identity.Name
                 product.User = await this.userHelper.GetUserByEmailAsync("jzuluaga55@gmail.com");
                 await this.productRepository.CreateAsync(product);
                 return RedirectToAction(nameof(Index));
             }
 
-            return View(product);
+            return View(view);
+        }
+
+        private Product ToProduct(ProductViewModel view, string path)
+        {
+            return new Product
+            {
+                Id = view.Id,
+                ImageUrl = path,
+                IsAvailable = view.IsAvailable,
+                LastPurchase = view.LastPurchase,
+                LastSale = view.LastSale,
+                Name = view.Name,
+                Price = view.Price,
+                Stock = view.Stock,
+                User = view.User
+            };
         }
 
         // GET: Products/Edit/5
